8a3b12a39d68d1069faf87fd5a2765c8c047ac00
Rustem <Rustem123918@mail.ru>
2022-11-20 20:09:35 +0300

    created scene loader

---
+++ Assets/_App/Scripts/Infrastructure/ICoroutineRunner.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace _App.Scripts.Infrastructure
+{
+    public interface ICoroutineRunner
+    {
+        Coroutine StartCoroutine(IEnumerator coroutine);
+    }
+}
