772b475afdbeb239163c3caefd34359eff8cc658
Rustem <Rustem123918@mail.ru>
2022-11-20 08:19:54 +0300

    create input service variants: for mobile and for standalone

---
+++ Assets/_App/Scripts/Services/Input/InputService.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace _App.Scripts.Services.Input
+{
+    public abstract class InputService : IInputService
+    {
+        protected const string Horizontal = "Horizontal";
+        protected const string Vertical = "Vertical";
+        private const string Button = "Fire";
+
+        public abstract Vector2 Axis { get; }
+
+        public bool IsAttackButtonUp() =>
+            SimpleInput.GetButtonUp(Button);
+
+        protected Vector2 SimpleInputAxis() =>
+            new Vector2(SimpleInput.GetAxis(Horizontal), SimpleInput.GetAxis(Vertical));
+    }
+}
---
+++ Assets/_App/Scripts/Services/Input/MobileInputService.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace _App.Scripts.Services.Input
+{
+    public class MobileInputService : InputService
+    {
+        public override Vector2 Axis => SimpleInputAxis();
+    }
+}
